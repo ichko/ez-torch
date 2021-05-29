575e2164b206cc142db4d7f2e7dcd167d2068335
Iliya Zhecev <iliya.zhechev@gmail.com>
2021-05-30 00:13:35 +0300

    Update example with model

--- tests/test_ez_torch.py
+++ tests/test_ez_torch.py
@@ -1,9 +1,10 @@
 import ez_torch as ez
 import matplotlib.pyplot as plt
 import pytest
+import torch.nn.functional as F
 import torchvision
 from ez_torch.data import get_mnist_dl
-from ez_torch.models import SpatialUVOffsetTransformer
+from ez_torch.models import Module, SpatialUVOffsetTransformer
 from ez_torch.vis import Fig
 
 
@@ -12,21 +13,43 @@ def test_version():
 
 
 def test_SpatialUVOffsetTransformer():
+    class Model(Module):
+        def __init__(self):
+            super().__init__()
+            self.feature_model = torchvision.models.resnet18(
+                pretrained=True, progress=False
+            )
+            self.transform = SpatialUVOffsetTransformer(
+                1000, uv_resolution_shape=(5, 5)
+            )
+
+        def criterion(self, y, y_target):
+            return F.binary_cross_entropy(y, y_target)
+
+        def forward(self, x):
+            X_features = self.feature_model(x.repeat([1, 3, 1, 1]))
+            X_transformed = self.transform([X_features, X])
+            return X_transformed
+
     train, test = get_mnist_dl(bs_train=100, bs_test=10, shuffle=False)
     X, y = next(iter(train))
 
-    feature_model = torchvision.models.resnet18(pretrained=True, progress=False)
+    model = Model()
     # feature_model.eval()
-
-    model = SpatialUVOffsetTransformer(1000, uv_resolution_shape=(5, 5))
-    X_features = feature_model(X.repeat([1, 3, 1, 1]))
-    X_transformed = model([X_features, X])
 
     fig = Fig(nr=1, nc=2, figsize=(10, 5))
 
     for i in range(100):
+        info = model.optim_step([X, y])
+        loss = info["loss"]
+        X_transformed = info["y_pred"]
+
+        print(loss)
+
+        # TODO: Fix animation updates
         fig[0].imshow(X.ez.grid(nr=10).channel_last)
         fig[1].imshow(X_transformed.ez.grid(nr=10).channel_last)
+        fig.update()
 
     plt.show()
 
