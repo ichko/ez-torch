83dc5c5b047066d4d83376cf6bf87fad9a66e1b0
Iliya Zhecev <iliya.zhechev@gmail.com>
2021-05-29 16:18:50 +0300

    Spatial offset test

--- tests/test_ez_torch.py
+++ tests/test_ez_torch.py
@@ -1,5 +1,10 @@
 import ez_torch as ez
+import matplotlib.pyplot as plt
+import pytest
+import torchvision
+from ez_torch.data import get_mnist_dl
 from ez_torch.models import SpatialUVOffsetTransformer
+from ez_torch.vis import Fig
 
 
 def test_version():
@@ -7,4 +12,24 @@ def test_version():
 
 
 def test_SpatialUVOffsetTransformer():
-    model = SpatialUVOffsetTransformer(1000, uv_resolution_shape=(10, 10))
+    train, test = get_mnist_dl(bs_train=100, bs_test=10, shuffle=False)
+    X, y = next(iter(train))
+
+    feature_model = torchvision.models.resnet18(pretrained=True, progress=False)
+    # feature_model.eval()
+
+    model = SpatialUVOffsetTransformer(1000, uv_resolution_shape=(5, 5))
+    X_features = feature_model(X.repeat([1, 3, 1, 1]))
+    X_transformed = model([X_features, X])
+
+    fig = Fig(nr=1, nc=2, figsize=(10, 5))
+
+    for i in range(100):
+        fig[0].imshow(X.ez.grid(nr=10).channel_last)
+        fig[1].imshow(X_transformed.ez.grid(nr=10).channel_last)
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    pytest.main()
