3f7881c3cb8a991e550d10fe9eab50b464fdfae8
Iliya Zhecev <iliya.zhechev@gmail.com>
2021-09-06 01:22:34 +0300

    Implement fig video saver

--- ez_torch/vis.py
+++ ez_torch/vis.py
@@ -1,5 +1,10 @@
+import os
+import uuid
+from datetime import datetime
+from shutil import copyfile
 from typing import Dict
 
+import ffmpeg
 import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib.pyplot import draw, pause
@@ -15,8 +20,21 @@ def unwrap(tensor):
     return tensor
 
 
+IMAGES_BASE_PATH = ".tmp/fig_imgs/"
+
+
 class Fig:
-    def __init__(self, nr=1, nc=1, ion=False, *args, **kwargs):
+    def __init__(
+        self,
+        nr=1,
+        nc=1,
+        ion=False,
+        realtime_render=True,
+        vid_path=None,
+        fps=30,
+        *args,
+        **kwargs,
+    ):
         if ion:
             plt.ion()
 
@@ -24,7 +42,16 @@ class Fig:
 
         self.axs = self.fig.subplots(nr, nc)
         self.plots_mem = {}
+        self.id = f"{datetime.now()}-{uuid.uuid4()}"
+        self.current_frame = 0
+        self.realtime_render = realtime_render
+
         # plt.close()
+
+        self.vid_args = vid_path, fps
+        self.fig_images_path = os.path.join(IMAGES_BASE_PATH, self.id)
+        self.current_image_path = os.path.join(self.fig_images_path, "current.png")
+        os.makedirs(self.fig_images_path, exist_ok=True)
 
     def __getitem__(self, index):
         ax = self.axs[index] if hasattr(self.axs, "__getitem__") else self.axs
@@ -79,5 +106,27 @@ class Fig:
         return Getattr()
 
     def update(self):
-        self.fig.canvas.draw()
-        self.fig.canvas.flush_events()
+        self._save_fig()
+
+        if self.realtime_render:
+            self.fig.canvas.draw()
+            self.fig.canvas.flush_events()
+
+    def _save_fig(self):
+        sequential_image_path = os.path.join(
+            self.fig_images_path, f"seq_{self.current_frame:06d}.png"
+        )
+        plt.savefig(self.current_image_path)
+        copyfile(self.current_image_path, sequential_image_path)
+
+        self.current_frame += 1
+
+    def __del__(self):
+        path, fps = self.vid_args
+        if path is not None:
+            ffmpeg.input(
+                f"{self.fig_images_path}/seq_*.png",
+                pattern_type="glob",
+                framerate=fps,
+                loglevel="error",
+            ).output(path).overwrite_output().run()
